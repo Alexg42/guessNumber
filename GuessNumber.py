219fee30cc28abeb89202861a413035dede15da2
Alex <alexg_42@hotmail.co.uk>
2017-09-18 22:14:18 +0100

    Refactor and input validation
    Added input validation - needs completing
    Started refactoring too

--- GuessNumber.py
+++ GuessNumber.py
@@ -1,27 +1,29 @@
 # This is a simple guess the number game
-#todo - input validation, re-factoring
+# todo - input validation, re-factoring
 
 import random
 
+maximum = 20
+maxGuesses = 6
 guessesTaken = 0
 
-print('Hello! What is your name?')
+myName = input('Hello! What is your name?\n')
 
-myName = input()
+while True:
+    try:
+        difficulty = int(input('Hi ' + myName + '! What difficulty would you like? Type a number.'
+                                                ' 1 = Easy, 2 = Medium, 3 = Hard\n'))
+    except ValueError:
+        print("Please enter a number.")
+        continue
+    else:
+        break
 
-print('Hi ' + myName + '! What difficulty would you like? Type a number. 1 = Easy, 2 = Medium, 3 = Hard')
-
-difficulty = int(input())
-
-if (difficulty == 1):
-    maximum = 20
-    maxGuesses = 6
-    
-elif (difficulty == 2):
+if difficulty == 2:
     maximum = 50
     maxGuesses = 8
-    
-elif (difficulty == 3):
+
+elif difficulty == 3:
     maximum = 100
     maxGuesses = 10
 
@@ -30,18 +32,14 @@ number = random.randint(1, maximum)
 print('Well, ' + myName + ', I am thinking of a number between 1 and ' + str(maximum) + '. Take a guess.')
 
 while guessesTaken < maxGuesses:
-    guess = input()
-    guess = int(guess)
+    guess = int(input())
     guessesTaken = guessesTaken + 1
-    if guess == number:
-        break
-    elif guess > number:
+    if guess > number:
         print('Your guess was too high!')
     elif guess < number:
         print('Your guess was too low!')
-    
-if guess == number:
-    print('Well done '+ myName +'! You have guessed correctly! You guessed the number in ' + str(guessesTaken) + ' guesses.')
-    
-else:
-    print('Unlucky. You did not guess the correct number. It was ' + str(number) + '.')
+    elif guess == number:
+        print('Well done ' + myName + '! You have guessed correctly! You guessed the number in ' + str(guessesTaken) +
+              ' guesses.')
+
+print('Unlucky. You did not guess the correct number. It was ' + str(number) + '.\n')
